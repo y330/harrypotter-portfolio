1e6d29f385fcfa71bbaa5f513fffbb966c1c342c
Yonah Aviv <avivyonah@gmail.com>
2021-08-23 21:41:19 -0400

    Updates

---
+++ _app/chunks/stores-22cfbdb9.js
@@ -0,0 +1,1 @@
+import{C as o}from"./vendor-35f37b0e.js";const e=o("Home");export{e as a};
---
+++ _app/pages/services.svelte-8eeffd1e.js
@@ -0,0 +1,1 @@
+import{S as s,i as r,s as e,I as t,J as a}from"../chunks/vendor-35f37b0e.js";import{a as o}from"../chunks/stores-22cfbdb9.js";function n(s,r,e){let n;return t(s,o,(s=>e(0,n=s))),a(o,n="Services",n),[]}class c extends s{constructor(s){super(),r(this,s,n,null,e,{})}}export{c as default};
