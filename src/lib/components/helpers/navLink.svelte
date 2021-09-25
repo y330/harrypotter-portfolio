7a49b3cad3064fd2f74b62de1b6685754d725bf5
Yonah Aviv <avivyonah@gmail.com>
2021-09-25 21:05:50 +0000

    done

--- src/lib/components/helpers/navLink.svelte
+++ src/lib/components/helpers/navLink.svelte
@@ -2,16 +2,16 @@
 	import ChevronLeft from 'tabler-icons-svelte/icons/ChevronLeft.svelte';
 	import ChevronRight from 'tabler-icons-svelte/icons/ChevronRight.svelte';
 	// ------------
-	export let active = false;
-	export let page = 'Home';
-	export let isAuthor = false;
+	export let active: boolean = false;
+	export let page: string = 'Home';
+	export let isAuthor: boolean = false;
 </script>
 
 <a
 	class="btn rounded-btn capitalize font-mono hover:text-primary-focus {active == true
 		? 'text-primary px-2 mx-2'
 		: 'px-10'}"
-	href="{if (isAuthor) {'https://yonah.ml'}else if (page == 'Home') {'/'} else {'/' + page.toLowerCase()}}"
+	href="{() => {if (isAuthor === true) {'https://yonah.ml'}else if (page == 'Home') {return '/'} else {return '/' + page.toLowerCase()}}"
 >
 	{#if isAuthor === true}
 		<ChevronLeft />
