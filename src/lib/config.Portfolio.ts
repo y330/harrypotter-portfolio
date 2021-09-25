eb0997e4c0f3b64bcd0dadfb70344c20e6ed5840
Yonah Aviv <avivyonah@gmail.com>
2021-09-25 16:32:03 -0400

    Update config.Portfolio.ts

--- src/lib/config.Portfolio.ts
+++ src/lib/config.Portfolio.ts
@@ -8,7 +8,7 @@ export default {
 			},
 			{
 				platform: 'discord',
-				url: 'https://discordapp.com/users/737540230957105254'
+				url: 'https://discord.com/users/737540230957105254'
 			}
 		],
 		introduction:
